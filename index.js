e8cd3e1fba8bae5f8975e0addc76af125684d06b
LukeChamley <lkchamley@gmail.com>
2022-08-08 17:43:53 -0600

    commit tree d5e2113f50d9a40ef04573a9001e367bd4a4620d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42){
+    return (blocks - 42)}
+    else if (blocks < 42)
+    return (42 - blocks)
+  }
+function distanceFromHqInFeet(feet){
+  return distanceFromHqInBlocks(feet) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination)*264
+    }
+    else {
+        return (destination - start)*264
+    }
+
+
+}
